799716d204730c0cbceffb4b1e23c524fe48286c
초록 <jygong@menuit.io>
2024-12-26 19:07:19 +0900

    좀 더 보완

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useRef  } from 'react'
+import { useRef, useState } from 'react'
 import { Canvas, useFrame, } from '@react-three/fiber'
 import { useGLTF, PositionalAudio, Text } from '@react-three/drei'
-
+import MyImage from "/logo_3.svg"
 
 export default function App({ ready }) {
   return (
     <>
-    <Canvas camera={{ position: [0, 2, 20], fov: 40 }}>
-      <fog attach="fog" args={['#cc7b32', 0, 500]} />
-      <Model ready={ready} />
-    </Canvas>
-    <div>
-      asdasdsadasdsadsadasdsa
-    </div>
+      <Canvas camera={{ position: [0, 2, 20], fov: 40 }}>
+        <fog attach="fog" args={['#cc7b32', 0, 500]} />
+        <Model ready={ready} />
+      </Canvas>
+      <div style={{ position: 'absolute', top: '24px', left: '24px' }}>
+        <div style={{ display: 'flex', alignItems: 'center', justifycontents: 'center', flexDirection: 'column', }}>
+          <img src={MyImage} alt="My Image" style={{ width: "180px" }} />
+          <div style={{ fontSize: 28, marginTop: 18 }}>
+            이별노트
+          </div>
+          <div style={{ fontSize: 14, marginTop: 0 }}>
+            오늘이 지나면 괜찮아질 당신
+          </div>
+        </div>
+      </div>
     </>
   )
 }
@@ -25,32 +33,95 @@ source: https://sketchfab.com/models/356c773c4b9a45d8b9d4aa04c60ecb27
 title: Red City
 */
 function Model({ ready }) {
-
+  const color = '#f89E56'
   const group = useRef();
-  const { nodes, materials } = useGLTF('/scene-draco.glb')
-  useFrame(() => (group.current.rotation.y += 0.003))
+  const [rotationDirection, setRotationDirection] = useState(1); // 1: 왼쪽, -1: 오른쪽
+  const { nodes, materials } = useGLTF('/scene-draco.glb');
+  const forgetWishString = ["고마웠어", "그동안 고생했어", "너는 소중한 사람이야", "시간이 너의 편", '이제 괜찮아'];
+  const catchWishString = ["보고싶어"];
+  let FareWellString = forgetWishString;
+  // useFrame(() => (group.current.rotation.y += 0.001))
+  useFrame(() => {
+    if (group.current) {
+      // 회전 업데이트
+      group.current.rotation.y += 0.001 * rotationDirection;
+
+      // 방향 전환 조건: 특정 각도에서 방향 변경
+      if (group.current.rotation.y > Math.PI / 4) {
+        setRotationDirection(-1); // 오른쪽으로 변경
+      } else if (group.current.rotation.y < -Math.PI / 4) {
+        setRotationDirection(1); // 왼쪽으로 변경
+      }
+    }
+  });
   return (
     <group ref={group} scale={0.001} position={[0, 0, -100]} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
+      <group rotation={[-Math.PI / 1.98, 0, 0]}>
         <group position={[-102253.52, -210688.86, -17050.52]}>
           <mesh material={materials.Scene_Root} geometry={nodes.mesh_0.geometry} />
           {/* <mesh material={materials.Scene_Root} geometry={nodes.mesh_1.geometry} /> */}
-          <mesh material={materials.Scene_Root} geometry={nodes.mesh_2.geometry} />
+          {/* <mesh material={materials.Scene_Root} geometry={nodes.mesh_2.geometry} /> */}
           <mesh material={materials.Scene_Root} geometry={nodes.mesh_3.geometry} />
         </group>
 
-        {/* 글씨 추가 */}
+        {/* 중앙 글씨 */}
         <Text
           position={[0, 30, 0]} // 위쪽으로 위치 조정
-          fontSize={10000} // 텍스트 크기
-          color="#8dbce0" // 글씨 색깔
+          fontSize={7000} // 텍스트 크기
+          color={color} // 글씨 색깔
           anchorX="center" // 가로 정렬
           anchorY="middle" // 세로 정렬
           font="/fonts/Tenada.ttf"
           rotation={[Math.PI / 2, 0, 0]} // X축으로 45도 회전
         >
-          고마웠어
+          {FareWellString[0]}
         </Text>
+        {/* 중앙 글씨 */}
+        <Text
+          position={[80000, 80000, 8000]} // 우측 위로 감
+          fontSize={5000} // 텍스트 크기
+          color={color}  // 글씨 색깔
+          anchorX="right" // 가로 정렬
+          anchorY="middle" // 세로 정렬
+          font="/fonts/Tenada.ttf"
+          rotation={[Math.PI / 2, 0, 0]} // X축으로 45도 회전
+        >
+          {FareWellString[1]}
+        </Text>
+        <Text
+          position={[-30000, 0, 20000]} // 우측 위로 감
+          fontSize={3000} // 텍스트 크기
+          color={color}  // 글씨 색깔
+          anchorX="right" // 가로 정렬
+          anchorY="middle" // 세로 정렬
+          font="/fonts/Tenada.ttf"
+          rotation={[Math.PI / 2, 0, 0]} // X축으로 45도 회전
+        >
+          {FareWellString[2]}
+        </Text>
+        <Text
+          position={[-20000, 150, 10000]} // 우측 위로 감
+          fontSize={3000} // 텍스트 크기
+          color={color}  // 글씨 색깔
+          anchorX="right" // 가로 정렬
+          anchorY="middle" // 세로 정렬
+          font="/fonts/Tenada.ttf"
+          rotation={[Math.PI / 2, 0, 0]} // X축으로 45도 회전
+        >
+          {FareWellString[3]}
+        </Text>
+        <Text
+          position={[20000, 1500, 20000]} // 우측 위로 감
+          fontSize={3000} // 텍스트 크기
+          color={color}  // 글씨 색깔
+          anchorX="right" // 가로 정렬
+          anchorY="middle" // 세로 정렬
+          font="/fonts/Tenada.ttf"
+          rotation={[Math.PI / 2, 0, 0]} // X축으로 45도 회전
+        >
+          {FareWellString[4]}
+        </Text>
+
         <group position={[100000, 120000, 2000]}>
           {ready && <PositionalAudio autoplay loop url="/zapsplat_icecream.mp3" distance={3} />}
         </group>
--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,31 +1,62 @@
-import { Suspense, cloneElement, useEffect, useState } from 'react'
-import { Footer } from '@pmndrs/branding'
+import { Suspense, cloneElement, useEffect, useState } from 'react';
+import { Footer } from '@pmndrs/branding';
+import styled from 'styled-components';
 
 function Ready({ setReady }) {
   useEffect(() => () => void setReady(true), [])
   return null
 }
+const Button = styled.button`
+  background: transparent;
+  border: 2px solid white;
+  border-radius: 12px;
+  color: white;
+  font-size: 16px;
+  padding: 30px 20px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
+
+  &:hover {
+    background-color: white;
+    color: #333;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default function Intro({ children }) {
   const [clicked, setClicked] = useState(false)
   const [ready, setReady] = useState(false)
+  const [level, setLevel] = useState(0);
   return (
     <>
       <Suspense fallback={<Ready setReady={setReady} />}>
         {cloneElement(children, { ready: clicked && ready })}
       </Suspense>
       <div className={`fullscreen bg ${ready ? 'ready' : 'notready'} ${clicked && 'clicked'}`}>
-        <div className="stack">
-          <a href="#" onClick={() => setClicked(true)}>
-            {!ready ? '기다리는 중' : '안녕하세요'}
+        <div className="stack" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 32, fontSize: 32 }}>
+          {level === 0 && (!ready ? '기다리는 중' : "누군가를 그리워하고 있나요?")}
+          {ready && <div className="fade-in" style={{ display: 'flex', alignItems: 'center', gap: 18 }}><a href="#" onClick={() => setClicked(true)}>
+            <Button style={{ fontSize: 18 }}>
+              그 사람을 잊고 싶어요.
+            </Button>
           </a>
+            <a href="#" onClick={() => setClicked(true)}>
+              <Button style={{ fontSize: 18 }}>
+                그 사람을 잡고 싶어요.
+              </Button>
+            </a></div>
+          }
+
         </div>
-        {/* <Footer
-          date="12월 26일"
+        <Footer
+          date="12 /26"
           year="2024"
           link1={<a href="https://github.com/pmndrs/drei">pmndrs/drei</a>}
           link2={<a href="https://codesandbox.io/s/e6bjz">s/e6bjz</a>}
-        /> */}
+        />
       </div>
     </>
   )
